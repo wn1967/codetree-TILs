f87dca9835deade1f7a73ef349bd730194fa6d4f
Judol <134105326+wn1967@users.noreply.github.com>
2024-06-09 23:21:09 +0900

    Title: 최소공배수 구하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240609/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240609/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,13 +2,11 @@ a,b = map(int, input().split())
 result = 0
 def xas(n,m):
     global result
-    abc = 500
 
     for i in range(max(n,m), n*m+1):
-        if i%n == 0 and i%m ==0 and i < abc:
-            abc=i
-            result = abc
-        
+        if i%n == 0 and i%m ==0:
+            result=i
+            break        
 xas(a,b)
 
 print(result)
