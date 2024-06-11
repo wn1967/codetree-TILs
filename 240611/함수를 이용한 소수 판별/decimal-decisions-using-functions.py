65d7a7b65ef84823cd560f4edaf7e407600fee60
Judol <134105326+wn1967@users.noreply.github.com>
2024-06-11 17:06:14 +0900

    Title: 함수를 이용한 소수 판별, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240611/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240611/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -11,14 +11,11 @@ def prime(number):
          blank.append(i)
 
 for i in range(a,b+1):
+    if a==b==1:
+        pass
+    
     number = i
     prime(number)
-    # for j in range(2, number):
-    #     if number % j == 0 :
-    #         result = 'False'            
-        
-    # if result == 'True':
-    #      blank.append(i)
 
 
 print(sum(blank))
