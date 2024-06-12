4ec18cd22765ed2ad4d19fb933191040f646491a
Judol <134105326+wn1967@users.noreply.github.com>
2024-06-12 17:10:25 +0900

    Title: 사칙연산 함수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240612/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,15 @@
+a,o,c = input().split()
+result = 0
+ab = int(a)
+cb = int(c)
+if  o == '+':
+    result == ab + cb
+elif  o == '-':
+    result = ab - cb
+elif  o == '*':
+    result = ab * cb
+elif  o == '/':
+    result = ab / cb
+
+
+print(a,o,c, '=', result)
