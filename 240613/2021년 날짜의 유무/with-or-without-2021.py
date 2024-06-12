3be8c9b6109b78ddc3145d6f2c707dd9aa1eb35f
Judol <134105326+wn1967@users.noreply.github.com>
2024-06-13 03:28:27 +0900

    Title: 2021년 날짜의 유무, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240613/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,36 @@
+a,b = map(int, input().split())
+
+def checkdate(a,b):
+    global result
+    if a == 2:
+        if b in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]:
+            result = 'Yes'
+            return result
+        else:
+            result = 'No'
+
+            return result
+
+    elif a in [1,3,5,7,8, 10,12]:
+        if b in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]:
+            result = 'Yes'
+
+            return result
+        else:
+            result = 'No'
+
+            return result
+
+    else:
+        if b in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]:
+            result = 'Yes'
+
+            return result
+        else:
+            result = 'No'
+            return result
+
+
+checkdate(a,b)
+
+print(result)
