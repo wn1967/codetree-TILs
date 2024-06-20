9685472db1efbaf04a437e13c04454c9382b6e8a
Judol <134105326+wn1967@users.noreply.github.com>
2024-06-20 18:13:31 +0900

    Title: palindrome 여부 판단하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
+++ 240620/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -1,19 +1,17 @@
 word = input()
 
 def padd(word):
-    if len(word)%2 == 0 :
-        print('No')
-    else:
-        result = 'True'
-        for i in range(int(len(word)/2)):
-            if word[i] == word[-(i+1)]:
-                result = 'True'
-            else:
-                result = 'False'
-    
-        if result == 'True':
-            print('Yes')
+    result = 'True'
+
+    for i in range(int(len(word)/2)):
+        if word[i] == word[-(i+1)]:
+            result = 'True'
         else:
-            print('No')
+            result = 'False'
+
+    if result == 'True':
+        print('Yes')
+    else:
+        print('No')
 
 padd(word)
