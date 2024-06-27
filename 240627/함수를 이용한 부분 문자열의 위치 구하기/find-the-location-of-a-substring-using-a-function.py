865a59e66b5110fe9479e0a5a8c1f7b1ae5969c4
Judol <134105326+wn1967@users.noreply.github.com>
2024-06-27 17:07:18 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240627/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240627/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -2,15 +2,18 @@ word = input()
 search = input()
 
 def check(word, search):
-    global result
-    for i in range(len(word)-1):
-        result = 0
+    result = -1  # 초기값을 -1로 설정
+    for i in range(len(word) - len(search) + 1):  # 검색 범위 조정
+        match_count = 0
         for j in range(len(search)):
-            if word[i+j] == search[j]:
-                result += 1        
-    if result == len(search):
-        print(i)
-    elif result < len(search):
-        print(-1)
+            if word[i + j] == search[j]:
+                match_count += 1
+            else:
+                break  # 일치하지 않으면 중지
+        if match_count == len(search):
+            result = i
+            break  # 일치하는 첫 번째 위치를 찾으면 중지
+
+    print(result)
 
 check(word, search)
