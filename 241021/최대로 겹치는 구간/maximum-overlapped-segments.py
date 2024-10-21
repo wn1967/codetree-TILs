454a0611ebb1c82ad37a4c58c578450bc260863e
Judol <134105326+wn1967@users.noreply.github.com>
2024-10-21 16:34:14 +0900

    Title: 최대로 겹치는 구간, Time: 110ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241021/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 241021/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,7 +1,9 @@
 n = int(input())
-array = [0]*101
+array = [0]*300
 for i in range(n):
     x,y = map(int, input().split())
+    x += 100
+    y += 100
     for j in range(x,y+1):
         array[j] += 1
 
