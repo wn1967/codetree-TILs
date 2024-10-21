2bde0735c551ccf1497f63988b8c7ed8eb389f86
Judol <134105326+wn1967@users.noreply.github.com>
2024-10-21 16:23:27 +0900

    Title: 블럭쌓는 명령2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241021/블럭쌓는 명령2/block-stacking-commands2.py
+++ 241021/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,7 +2,12 @@ N,K = map(int, input().split())
 array = [0]*N
 for i in range(K):
     A,B = map(int, input().split())
+    # if A < 0 or B >= N or A > B:
+    #     print("Invalid input: A and B must be within the range of 0 to N-1.")
+    #     exit()
     for j in range(A,B+1):
-        array[j] += 1
+        if j < N:
+
+            array[j] += 1
 value = max(array)
 print(value)
