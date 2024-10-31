7ae26a989b32be5ec967bafa3ffbc384e2801d34
Judol <134105326+wn1967@users.noreply.github.com>
2024-10-31 18:17:16 +0900

    Title: 연속되는 수 2, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 241031/연속되는 수 2/continuous-number2.py
+++ 241031/연속되는 수 2/continuous-number2.py
@@ -1,19 +1,44 @@
-N = int(input())
-blank = []
-for i in range(N):
-    aa = int(input())
-    blank.append(aa)
+# N = int(input())
+# blank = []
+# for i in range(N):
+#     aa = int(input())
+#     blank.append(aa)
 
-blank2 = []
+# blank2 = []
 
-cnt = 0
-for j in range(N):
-    cnt += 1
-    if blank[j] == blank[j-1]:
-        
-        blank2.append(cnt)
-    elif blank[j] != blank[j-1]:
-        blank2.append(cnt)
-        cnt = 1
+# cnt = 1
+# for j in range(N):
+    
 
-print(max(blank2))
+#     if blank[j] ==0 or blank[j] == blank[j-1]:
+#         cnt += 1
+
+
+#     elif blank[j] != blank[j-1]:
+#         cnt = 0
+
+#     blank2.append(cnt)
+
+#     print(blank2)    
+def max_consecutive_numbers(numbers):
+  max_count = 0
+  current_count = 1
+  for i in range(1, len(numbers)):
+    if numbers[i] == numbers[i-1]:
+      current_count += 1
+    else:
+      max_count = max(max_count, current_count)
+      current_count = 1
+
+  # 마지막 연속된 숫자를 처리
+  max_count = max(max_count, current_count)
+
+  return max_count
+
+# 입력 받기
+n = int(input())
+numbers = [int(input()) for _ in range(n)]
+
+# 최대 연속 횟수 계산 및 출력
+result = max_consecutive_numbers(numbers)
+print(result)
