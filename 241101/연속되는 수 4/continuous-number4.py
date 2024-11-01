78dabbcdac33c236bad0dc9becad2f972f517f19
Judol <134105326+wn1967@users.noreply.github.com>
2024-11-01 10:26:53 +0900

    Title: 연속되는 수 4, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241101/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = []
+for i in range(N):
+    arr.append(int(input()))
+
+maxvalue = 0
+count = 1
+for j in range(N):
+    
+    if arr[j] > arr[j-1]:
+        count += 1
+        maxvalue = max(count, maxvalue)
+    else:
+        count = 1
+        maxvalue = max(count, maxvalue)
+    
+    print(maxvalue)
