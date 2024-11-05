eadfad59239662fe8d50d90e89c4e7d150284f01
Judol <134105326+wn1967@users.noreply.github.com>
2024-11-05 13:50:28 +0900

    Title: 만나는 그 순간, Time: 170ms, Memory: 55MB, Status: Passed - CodeTree

--- 241105/만나는 그 순간/the-moment-we-meet.py
+++ 241105/만나는 그 순간/the-moment-we-meet.py
@@ -1,54 +1,94 @@
-N,M = map(int, input().split())
-check1 = []
-check11 = []
-check2 = []
-check22 = []
+# N,M = map(int, input().split())
+# check1 = []
+# check11 = []
+# check2 = []
+# check22 = []
 
-start = 1000
-count = 0
-for i in range(N):
+# start = 1000
+# count = 0
+# for i in range(N):
+#     d, t = input().split()
+#     t = int(t)
+#     for j in range(1,t+1):
+#         if d == 'R':
+#             start += 1
+#             check1.append(start)
+#             count += 1
+#             check11.append(count)
+#         else:
+#             start -= 1
+#             check1.append(start)
+#             count += 1
+#             check11.append(count)
+
+# start = 1000
+# count = 0
+
+# for i in range(M):
+#     d, t = input().split()
+#     t = int(t)
+#     for j in range(1,t+1):
+#         if d == 'R':
+#             start += 1
+#             check2.append(start)
+#             count += 1
+#             check22.append(count)
+#         else:
+#             start -= 1
+#             check2.append(start)
+#             count += 1
+#             check22.append(count)
+# result  = 0
+# answer = 0
+# for z in range(len(check11)):
+#     if check1[z] == check2[z]:
+#         result = 1
+#         answer = z+1
+#         break
+#     else:
+#         result = -1     
+
+# if result == 1:
+#     print(answer)
+# else:
+#     print(-1)
+
+N, M = map(int, input().split())
+A_moves = []
+B_moves = []
+
+for _ in range(N):
     d, t = input().split()
     t = int(t)
-    for j in range(1,t+1):
-        if d == 'R':
-            start += 1
-            check1.append(start)
-            count += 1
-            check11.append(count)
-        else:
-            start -= 1
-            check1.append(start)
-            count += 1
-            check11.append(count)
+    A_moves.append((d, t))
 
-start = 1000
-count = 0
-
-for i in range(M):
+for _ in range(M):
     d, t = input().split()
     t = int(t)
-    for j in range(1,t+1):
-        if d == 'R':
-            start += 1
-            check2.append(start)
-            count += 1
-            check22.append(count)
-        else:
-            start -= 1
-            check2.append(start)
-            count += 1
-            check22.append(count)
-result  = 0
-answer = 0
-for z in range(len(check11)):
-    if check1[z] == check2[z]:
-        result = 1
-        answer = z+1
+    B_moves.append((d, t))
+
+A_pos = B_pos = 1000
+time_A = time_B = 0
+
+A_path = {}
+B_path = {}
+
+for d, t in A_moves:
+    for _ in range(t):
+        A_pos += 1 if d == 'R' else -1
+        time_A += 1
+        A_path[time_A] = A_pos
+
+for d, t in B_moves:
+    for _ in range(t):
+        B_pos += 1 if d == 'R' else -1
+        time_B += 1
+        B_path[time_B] = B_pos
+
+meeting_time = -1
+for t in A_path:
+    if A_path[t] == B_path.get(t):
+        meeting_time = t
         break
-    else:
-        result = -1     
 
-if result == 1:
-    print(answer)
-else:
-    print(-1)
+print(meeting_time)
